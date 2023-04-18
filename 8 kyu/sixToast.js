1385b179e6d12506d754070ff55c31d49c60801a
Karolina Dbaj <k.dbaj@outlook.com>
2023-04-18 13:36:09 +0200

    Making Six Toast

---
+++ 8 kyu/sixToast.js
@@ -0,0 +1,11 @@
+function sixToast(num) {
+  if (num === 6) {
+    return 0;
+  } else {
+    return Math.abs(num - 6);
+  }
+}
+
+function sixToast(num) {
+  return num === 6 ? 0 : Math.abs(num - 6);
+}
